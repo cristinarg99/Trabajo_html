f8fd47f18d2c4d2971d0376e1d0290b6f692b74a
Adrian <adriansansi@gmail.com>
2024-06-05 08:47:35 +0200

    protomostrador de citas

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function updateClock() {
 }
 setInterval(updateClock, 1000);
 updateClock(); // Llamada inicial para que no haya retraso
+
+// Generador de citas aleatorias
+const quotes = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+    "Frase 5"
+];
+
+function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    const quoteElement = document.getElementById('quote');
+    quoteElement.textContent = quote;
+}
+
+setInterval(generateQuote, 10000); // Cambia la cita cada 10 segundos
+generateQuote();
