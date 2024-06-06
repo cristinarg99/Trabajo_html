af40df38897f61144d53238fd1e8484ceba25a33
cristinarg99 <cristinarg@unav.es>
2024-06-06 08:05:05 +0200

    Frases super chulis

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ updateClock(); // Llamada inicial para que no haya retraso
 
 // Generador de citas aleatorias
 const quotes = [
-    "Frase 1Frase 1Frase 1Frase blablablabla",
-    "Frase 2",
-    "Frase 3",
-    "Frase 4",
-    "Frase 5"
+    "“The key to success with AI is not just having the right data, but also asking the right questions.” – Ginni Rometty",
+    "“The idea that machines can’t do things humans can is a pure myth.” – Marvin Minsky",
+    "“AI will not make us obsolete, but it will make us superhuman.” – Max Tegmark",
+    "“Competition for AI superiority at national level is the most likely cause of WW3.” — Elon Musk",
+    "“A year spent in artificial intelligence is enough to make one believe in God.” —Alan Perlis"
 ];
 
 function generateQuote() {
